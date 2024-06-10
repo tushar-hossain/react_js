de092ac6a2a5d3268c0d8b4003853e67f4045499
Tushar_Hossain <tusharhossain455@gmail.com>
2024-06-11 01:02:15 +0600

    how to create and use react component

---
+++ 5 how to create and use react component/react-component/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ 5 how to create and use react component/react-component/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <div className="card">
+      <h1 className="card-header">Todo App</h1>
+      <p className="card-title">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      <p className="card-footer">Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+}
+
+export default Card;
