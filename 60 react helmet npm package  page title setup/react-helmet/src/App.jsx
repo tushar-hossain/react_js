cf2dfb2b617faaf9283ed4f11f2d9ee48907b14d
Tushar-Hossain <tusharhossain455@gmail.com>
2024-08-04 16:44:58 +0600

    page title setup

---
+++ 60 react helmet npm package  page title setup/react-helmet/src/App.jsx
@@ -0,0 +1,14 @@
+import PageTitle from "./component/PageTitle";
+import Shope from "./page/Shope";
+
+function App() {
+  return (
+    <div>
+      <PageTitle title="Home" />
+      <Shope />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ 60 react helmet npm package  page title setup/react-helmet/src/page/Shope.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageTitle from "../component/PageTitle";
+
+const Shope = () => {
+  return (
+    <div>
+      <PageTitle title="Shope" />
+      <a href="#" target="_blank">
+        <h1>Shope Page</h1>
+      </a>
+    </div>
+  );
+};
+
+export default Shope;
